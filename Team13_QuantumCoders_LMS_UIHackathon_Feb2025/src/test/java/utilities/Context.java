5a12696c8cb8b0e979f35cd133682e78d70d5b86
Anupriya <anupriyak2050@gmail.com>
2025-02-24 22:49:46 -0500

    Update Context.java

--- Team13_QuantumCoders_LMS_UIHackathon_Feb2025/src/test/java/utilities/Context.java
+++ Team13_QuantumCoders_LMS_UIHackathon_Feb2025/src/test/java/utilities/Context.java
@@ -4,26 +4,25 @@ import org.openqa.selenium.WebDriver;
 
 import driverFactory.DriverFactory;
 import pageObjects.ProgramPageTwo;
-//import pageObjects.AddNewClass;
-//import pageObjects.AddNewClassPopup;
-//import pageObjects.Batch;
-//import pageObjects.BatchPage;
-//import pageObjects.BatchPageValidations;
-//import pageObjects.ClassPageValidation;
-//import pageObjects.ClassPg;
-//import pageObjects.DashboardPage;
+import pageObjects.ClassPage;
 import pageObjects.LoginPage;
 import pageObjects.ProgramPage;
 public class Context {
 
 	private DriverFactory driverFactory;
 	private LoginPage loginPage;
+	private ClassPage classPage;
+        private LogOutPage logoutPage;
+	private HomePage homePage;
 	private ProgramPage program;
 	private ProgramPageTwo programPage;
 
 	public Context() {
 		driverFactory = new DriverFactory();
 		loginPage = new LoginPage(driverFactory.getDriver());
+		classPage = new ClassPage(driverFactory.getDriver());
+		logoutPage = new LogOutPage(driverFactory.getDriver());
+		homePage = new HomePage(driverFactory.getDriver());
 		program = new ProgramPage(driverFactory.getDriver());
 		programPage= new ProgramPageTwo(driverFactory.getDriver());
 	}
@@ -38,6 +37,17 @@ public class Context {
 	public  ProgramPage ProgramPage  () {
 		return program;
 	}
+	public ClassPage getClassPage() {
+		return classPage;
+	}
+        public LogOutPage getLogOutPage() {
+		return logoutPage;
+	}
+	
+	public HomePage getHomePage() {
+		return homePage;
+	}
+
 	
 	public ProgramPageTwo getEditProgramPage() {
 		return programPage;
